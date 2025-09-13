258a56cf251bcd0a17bf18a85186591f6650feda
weewx-auto <weewx@local>
2025-09-13 00:20:01 -0300

    Auto-update: 2025-09-13 00:20:01

--- js/belchertown.js
+++ js/belchertown.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function() {
         } else if (window.location.search.indexOf('?theme=auto') === 0) {
             belchertown_debug("Theme: Setting auto theme because of URL override");
             sessionStorage.setItem('theme', 'auto')
-            autoTheme(18, 45, 06, 59)
+            autoTheme(18, 46, 06, 57)
         }
     }
 
