bd0e5ab38637ea29d5c7fe5ff8c3221110f3d3cc
weewx-auto <weewx@local>
2025-10-29 00:20:01 -0300

    Auto-update: 2025-10-29 00:20:01

--- js/belchertown.js
+++ js/belchertown.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function() {
         } else if (window.location.search.indexOf('?theme=auto') === 0) {
             belchertown_debug("Theme: Setting auto theme because of URL override");
             sessionStorage.setItem('theme', 'auto')
-            autoTheme(19, 22, 05, 57)
+            autoTheme(19, 23, 05, 56)
         }
     }
 
