1bdc70b772d7ea9a95c574cf5d79cca2af6ecd19
root <arielpabogado@gmail.com>
2026-07-08 00:00:01 -0300

    Auto-update: 2026-07-08 00:00:01

--- js/belchertown.js
+++ js/belchertown.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function() {
         } else if (window.location.search.indexOf('?theme=auto') === 0) {
             belchertown_debug("Theme: Setting auto theme because of URL override");
             sessionStorage.setItem('theme', 'auto')
-            autoTheme(17, 59, 08, 03)
+            autoTheme(17, 59, 08, 02)
         }
     }
 
